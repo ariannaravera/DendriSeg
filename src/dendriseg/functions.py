d830e523d6412cc6719a7d512af1e6d8b96eb9e0
Arianna Ravera <ariannaravera22@gmail.com>
2024-06-14 14:28:05 +0200

    Add intersections' dots

--- src/dendriseg/functions.py
+++ src/dendriseg/functions.py
@@ -15,8 +15,7 @@ from PIL import ImageColor
 import xlsxwriter
 from scipy import ndimage
 
-data_path = "/Users/aravera/Documents/DNF_Bagni/Giorgia/data/NeuroniDIV19_40xtiles.tif"
-res_path = "/Users/aravera/Documents/DNF_Bagni/Giorgia/results_test"
+res_path = "/Users/aravera/Documents/PROJECTS/DNF_Bagni/Giorgia/results/new_results"
 
 
 def open_lif(file_path, saving_dir):
@@ -117,19 +116,19 @@ def crop(image, image_mask, roi_ids, roi_image, output_path, image_name):
     cropped_areas = []
     print("Cropping the "+str(len(roi_ids))+" ROIs drawn in "+output_path)
     for roi_id in roi_ids:
-        roi_mask = np.zeros(image.shape, dtype=np.uint8)
-        roi_mask[roi_image == roi_id] = 1
-        masked_img = cv2.bitwise_and(image, image, mask = roi_mask)
-        masked_image_mask = cv2.bitwise_and(image_mask, image_mask, mask = roi_mask)
+        rointersection_mask = np.zeros(image.shape, dtype=np.uint8)
+        rointersection_mask[roi_image == roi_id] = 1
+        masked_img = cv2.bitwise_and(image, image, mask = rointersection_mask)
+        masked_image_mask = cv2.bitwise_and(image_mask, image_mask, mask = rointersection_mask)
         # Crop the black border of the mask and the masked image
-        y_nonzero, x_nonzero = np.nonzero(roi_mask)
+        y_nonzero, x_nonzero = np.nonzero(rointersection_mask)
         cropped_image = masked_img[np.min(y_nonzero):np.max(y_nonzero), np.min(x_nonzero):np.max(x_nonzero)]
         cropped_image_mask = masked_image_mask[np.min(y_nonzero):np.max(y_nonzero), np.min(x_nonzero):np.max(x_nonzero)]
-        cropped_roi_mask = roi_mask[np.min(y_nonzero):np.max(y_nonzero), np.min(x_nonzero):np.max(x_nonzero)]
+        cropped_rointersection_mask = rointersection_mask[np.min(y_nonzero):np.max(y_nonzero), np.min(x_nonzero):np.max(x_nonzero)]
         cropped_area = np.zeros((3, cropped_image.shape[0], cropped_image.shape[1]))
         cropped_area[0] = cropped_image
         cropped_area[1] = cropped_image_mask
-        cropped_area[2] = cropped_roi_mask
+        cropped_area[2] = cropped_rointersection_mask
         cropped_areas.append(cropped_area)
         tifffile.imwrite(os.path.join(output_path, image_name+'_ROI'+str(roi_id)+'.tif'), cropped_area)
     msg = QMessageBox() 
@@ -157,15 +156,14 @@ def open_file(file_path):
 
 
 def sholl_analysis(output_path, name, center, image, mask, scale):
-    print(output_path, name, center, image, mask, scale)
     if len(list(np.unique(center))) != 2:
         print('WARNING! Select only one center')
         return
     center[center != 0] = 255
     center = center.astype('uint8')
     mass_x, mass_y = np.where(center == 255)
-    cent_x = int(np.average(mass_x))
-    cent_y = int(np.average(mass_y))
+    cent_y = int(np.average(mass_x))
+    cent_x = int(np.average(mass_y))
 
     # radius = 5um x 10 times -> we need to scale um to pixel and then we define the range: from 5 to 100 (it is enough for all the images) with step 20
     radius = np.arange(int(scale*5), int(scale*100), int(scale*5))
@@ -179,11 +177,10 @@ def sholl_analysis(output_path, name, center, image, mask, scale):
     colors = mcp.gen_color(cmap="rainbow", n=20)
     # Add the sholl circles
     for i, rad in enumerate(radius):
-        cv2.circle(image3D, center=(cent_y, cent_x), radius=rad, color=ImageColor.getcolor(colors[i], "RGB"), thickness=1)
-        cv2.circle(circles_mask, center=(cent_y, cent_x), radius=rad, color=(i+1)*2, thickness=1)
+        cv2.circle(image3D, center=(cent_x, cent_y), radius=rad, color=ImageColor.getcolor(colors[i], "RGB"), thickness=1)
+        cv2.circle(circles_mask, center=(cent_x, cent_y), radius=rad, color=(i+1)*2, thickness=1)
     
-    cv2.circle(image3D, center=(cent_y, cent_x), radius=1, color=(0,100,255), thickness=1)
-    cv2.imwrite(os.path.join(os.path.dirname(str(output_path)), "sholl_"+name+".jpg"), image3D)
+    cv2.circle(image3D, center=(cent_x, cent_y), radius=1, color=(0,100,255), thickness=1)
     
     workbook = xlsxwriter.Workbook(os.path.join(os.path.dirname(str(output_path)), "sholl_"+name+".xlsx"))
     worksheet = workbook.add_worksheet()
@@ -197,13 +194,22 @@ def sholl_analysis(output_path, name, center, image, mask, scale):
         intersections[intersections == i] = 0
     for i, col in enumerate(np.unique(intersections)):
         if col != 0:
-            i_mask = np.zeros(intersections.shape, dtype=np.uint8)
-            i_mask[intersections==col] = 1
-            contours, _ = cv2.findContours(i_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            intersection_mask = np.zeros(intersections.shape, dtype=np.uint8)
+            intersection_mask[intersections==col] = 1
+
+            contours, _ = cv2.findContours(intersection_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
             worksheet.write(row, 0, i)
             worksheet.write(row, 1, len(contours))
             row += 1
-    worksheet.close()
+
+            for c in contours:
+                meanx = np.median([i[0][0] for i in c])
+                meany = np.median([i[0][1] for i in c])
+                cv2.circle(intersection_mask, (int(meanx),int(meany)), 1, col, -1)
+
+    cv2.imwrite(os.path.join(os.path.dirname(str(output_path)), "sholl_"+name+".jpg"), image3D)
+    workbook.close()
 
     print("Analysis saved in "+str(output_path))
     msg = QMessageBox() 
@@ -211,7 +217,6 @@ def sholl_analysis(output_path, name, center, image, mask, scale):
     msg.setText("Analysis saved!")
     msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     msg.exec_()
-
 
 
 if __name__ == "__main__":
@@ -227,5 +232,5 @@ if __name__ == "__main__":
     #img1 = tifffile.imread('/Users/aravera/Documents/PROJECTS/DNF_Bagni/Giorgia/data/new_data/extracted/40x WT GFP DIV8 210524_img1_ch0_scale=3+52.tif')
     #segment(img1)
     
-    img = tifffile.imread('/Users/aravera/Documents/PROJECTS/DNF_Bagni/Giorgia/results/new_results/40x WT GFP DIV8 210524_img1_ch2_scale=3+52_ROI1.tif')
-    sholl_analysis(res_path, '40x WT GFP DIV8 210524_img1_ch2_scale=3+52_ROI1', center, image, mask, scale)
+    
+    #sholl_analysis_test('/Users/aravera/Documents/PROJECTS/DNF_Bagni/Giorgia/results/new_results/40x WT GFP DIV8 210524_img1_ch2_scale=3+52_ROI1.tif')
